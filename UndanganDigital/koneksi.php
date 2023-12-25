bb698daa135dc19f88d117a5d0dc9b522eef2b90
Hamasif <150764266+Hamasif@users.noreply.github.com>
2023-12-25 14:00:14 +0700

    Update koneksi.php

--- UndanganDigital/koneksi.php
+++ UndanganDigital/koneksi.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL ^ E_NOTICE AND E_DEPRECATED);
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$db   = 'undangan';
+$db   = 'project';
 
 $conn = mysqli_connect($host,$user,$pass,$db);
 ?>
